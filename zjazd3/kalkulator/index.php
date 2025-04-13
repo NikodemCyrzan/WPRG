9e691e6e55ffd5e4b0aa81e9b89b44a45b2b3f19
Nikodem Cyrzan <85363474+NikodemCyrzan@users.noreply.github.com>
2025-04-13 13:05:15 +0200

    Create index.php

---
+++ zjazd3/kalkulator/index.php
@@ -0,0 +1,63 @@
+<?php
+include "add.php";
+include "dis.php";
+include "mul.php";
+include "div.php";
+
+$res = "";
+
+if (isset($_POST["submit"]) && isset($_POST["a"]) && isset($_POST["b"])  && isset($_POST["op"])) {
+    $operation = $_POST["op"];
+
+    switch ($operation) {
+        case "+":
+            $res = add($_POST["a"], $_POST["b"])
+            break;
+        case "-":
+            $res = dis($_POST["a"], $_POST["b"])
+            break;
+        case "*":
+            $res = mul($_POST["a"], $_POST["b"])
+            break;
+        case "/":
+            if ($_POST["b"] == 0) {
+                $res = "Nie dziel przez 0";
+            } else {
+                $res = div($_POST["a"], $_POST["b"])
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head></head>
+
+<body>
+    <form action="index.php" method="post">
+        <input type="number" name="a">
+        <br>
+        <input type="number" name="b">
+        <br>
+        <select name="op">
+            <option value="+" default>+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select>
+        <br><br>
+        <input type="submit" value="submit" name="submit">
+        <br>
+        <div>
+            <?php
+            echo $res;
+            ?>
+        </div>
+    </form>
+</body>
+
+</html>
